0922bd4a0513b9c659547afda0446f48998df0a9
EC2 Default User <ec2-user@ip-172-31-17-201.ec2.internal>
2019-02-13 22:48:47 +0000

    part3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,62 +11,38 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # @movies = Movie.all
-    session[:ratings] = params[:ratings] if params[:ratings]
-    session[:sort] = params[:sort] if params[:sort]
-
-    # redirect to RESTful path if session contains more info than provided in params
-    if (!params[:ratings] && session[:ratings]) || (!params[:sort] && session[:sort])
-      redirect_to movies_path(ratings: session[:ratings], sort: session[:sort])
-    end
-
-    query_base = Movie
-
-    if session[:ratings]
-      query_base = query_base.scoped(:conditions => { :rating => session[:ratings].keys })
-    end
-
-    if session[:sort]
-      query_base = query_base.scoped(:order => session[:sort])
-    end
-
-    @movies = query_base.all
-
+    @movies = Movie.all
     @all_ratings = Movie.all_ratings
 
-    if session[:ratings]
-      @selected_ratings = session[:ratings]
+    if params[:sort]
+      @sorted = params[:sort]
     else
-      @selected_ratings = {}
+      @sorted = session[:sort]
+    end
+
+    if params[:commit] == 'Refresh'
+      if params[:ratings]
+        @rating_filter = params[:ratings].keys
+      else
+        @rating_filter = @all_ratings
+      end
+    else
+      if session[:ratings]
+        @rating_filter = session[:ratings]
+      else
+        @rating_filter = @all_ratings
+      end
     end
     
+    if params[:sort] != session[:sort]
+      session[:sort] = @sorted
+    end
+    if params[:ratings] != session[:ratings]
+      session[:ratings] = @rating_filter
+    end
     
-    
-    
-    
-    # if params[:commit] == 'Refresh'
-    #   if params[:ratings]
-    #     @rating_filter = params[:ratings].keys
-    #   else
-    #     @rating_filter = @all_ratings
-    #   end
-    # else
-    #   if session[:ratings]
-    #     @rating_filter = session[:ratings]
-    #   else
-    #     @rating_filter = @all_ratings
-    #   end
-    # end
-    
-    # if params[:sort] != session[:sort]
-    #   session[:sort] = @sorted
-    # end
-    # if params[:ratings] != session[:ratings]
-    #   session[:ratings] = @rating_filter
-    # end
-    
-    # @movies = @movies.sorting(@sorted)
-    # @movies = @movies.with_ratings(@rating_filter)
+    @movies = @movies.sorting(@sorted)
+    @movies = @movies.with_ratings(@rating_filter)
     
   end
 

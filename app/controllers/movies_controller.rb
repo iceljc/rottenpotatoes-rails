84904ea34cf177f4a2fa54a7a4d60f0f23f5a2c8
EC2 Default User <ec2-user@ip-172-31-17-201.ec2.internal>
2019-02-14 02:37:08 +0000

    part3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     @all_ratings = Movie.all_ratings
     
-    if params[:sort]==nil && params[:ratings]==nil && (session[:sort] || session[:ratings])
+    if (params[:sort]==nil && session[:sort]) || (params[:ratings]==nil && session[:ratings])
       flash.keep
       redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
     end

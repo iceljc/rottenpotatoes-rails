ae9c58b01f110560eea6c3c35d81e59aa3add28f
EC2 Default User <ec2-user@ip-172-31-17-201.ec2.internal>
2019-02-15 20:36:43 +0000

    part3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,64 +14,63 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     @all_ratings = Movie.all_ratings
     
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'title'
-      ordering,@title_header = {:title => :asc}, 'hilite'
-    when 'release_date'
-      ordering,@date_header = {:release_date => :asc}, 'hilite'
-    end
-    @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
-    
-    if @selected_ratings == {}
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
-    end
-    
-    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
-    
-    
-    # @movies = Movie.all
-    # @all_ratings = Movie.all_ratings
-    
-    # if params[:sort]
-    #   @sorted = params[:sort]
-    # else
-    #   @sorted = session[:sort]
+    # sort = params[:sort] || session[:sort]
+    # case sort
+    # when 'title'
+    #   ordering,@title_header = {:title => :asc}, 'hilite'
+    # when 'release_date'
+    #   ordering,@date_header = {:release_date => :asc}, 'hilite'
     # end
-
-    # if params[:commit] == 'Refresh'
-    #   if params[:ratings]
-    #     @rating_filter = params[:ratings].keys
-    #   else  # press 'Refresh' while selecting nothing
-    #     @rating_filter = @all_ratings
-    #     @sorted = nil
-    #   end
-    # else
-    #   if params[:ratings]
-    #     @ratings_filter = (params[:ratings].is_a?(Hash)) ? params[:ratings].keys : params[:ratings]
-    #   else
-    #     if session[:ratings]
-    #       @rating_filter = session[:ratings]
-    #     else
-    #       @rating_filter = @all_ratings
-    #     end
-    #   end
+    # @selected_ratings = params[:ratings] || session[:ratings] || {}
+    
+    # if @selected_ratings == {}
+    #   @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
     # end
     
     # if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-    #   session[:sort] = @sorted
-    #   session[:ratings] = @rating_filter
-    #   redirect_to :sort => @sorted, :ratings => @rating_filter and return
+    #   session[:sort] = sort
+    #   session[:ratings] = @selected_ratings
+    #   redirect_to :sort => sort, :ratings => @selected_ratings and return
     # end
+    # @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
     
-    # @movies = @movies.sorting(@sorted)
-    # @movies = @movies.with_ratings(@rating_filter)
+    
+    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    
+    if params[:sort]
+      @sorted = params[:sort]
+    else
+      @sorted = session[:sort]
+    end
+
+    if params[:commit] == 'Refresh'
+      if params[:ratings]
+        @rating_filter = params[:ratings].keys
+      else  # press 'Refresh' while selecting nothing
+        @rating_filter = @all_ratings
+        @sorted = nil
+      end
+    else
+      if params[:ratings]
+        @ratings_filter = (params[:ratings].is_a?(Hash)) ? params[:ratings].keys : params[:ratings]
+      else
+        if session[:ratings]
+          @rating_filter = session[:ratings]
+        else
+          @rating_filter = @all_ratings
+        end
+      end
+    end
+    
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = @sorted
+      session[:ratings] = @rating_filter
+      redirect_to :sort => @sorted, :ratings => @rating_filter and return
+    end
+    
+    @movies = @movies.sorting(@sorted)
+    @movies = @movies.with_ratings(@rating_filter)
 
   end
 
